40f2d5b6ad3c7bb1a119ef359549fc2fd46b5629
jordandexter-pattlabs <jordan.dexter@pattlabs.com>
2025-11-18 09:34:42 -0800

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,18 +6,15 @@ export const metadata: Metadata = {
   title: "Jordan Dexter | Web Development Portfolio",
 };
 
-export const gtmScriptHead = `<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MQZXHH7G');</script>
-<!-- End Google Tag Manager -->`
+export const gtmScriptHead = `<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VL52QLRR5K"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-export const gtmScriptBody = `<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQZXHH7G"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->`
+  gtag('config', 'G-VL52QLRR5K');
+</script>`
 
 export default function RootLayout({
   children,
@@ -28,14 +25,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script dangerouslySetInnerHTML={{ __html: gtmScriptHead }} />
-
-
       </head>
       <body
         className={`flex bg-background flex-col min-w-[375px] antialiased hidden`}
       >
-        <script dangerouslySetInnerHTML={{ __html: gtmScriptBody }} />
-
         <Header />
         {children}
       </body>
