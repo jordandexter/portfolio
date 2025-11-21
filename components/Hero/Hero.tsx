f0c83fafae724fcb2ddd1c2b8a4cf633270c3908
jordandexter-pattlabs <jordan.dexter@pattlabs.com>
2025-11-21 09:05:17 -0800

    pushin

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useRef, RefObject, useEffect } from "react"
 import { motion, useScroll, useTransform } from "framer-motion"
+import HeroMobileHeader from "./HeroMobile";
 
 const SHOW_CONTAINER_NAMES = false
 
@@ -38,62 +39,69 @@ export default function HeroHeader({
     }
 
     return (
-        <motion.div className="sticky top-0 z-0 flex gap-10 flex-col md:flex-row md:h-screen h-[calc(100vh-155px)] w-full pt-15" style={{
-            opacity: opacityScale,
-            scale: scaleScale,
-            filter: blurScale && useTransform(blurScale, (b) => `blur(${b})`)
-        }}>
-            {SHOW_CONTAINER_NAMES && <p className="absolute text-red-500 font-bold">container 1</p>}
-            <div className="flex items-center justify-center flex-col flex-1 rounded-xl min-h-100 gap-8">
-                <div className="flex  h-50 w-50 md:h-50 md:w-50 fade-in rounded-full overflow-hidden justify-center items-center bg-white"
-                    style={{
-                        animationDuration: '1000ms',
-                    }}>
-                    <img className="object-cover h-50 w-50"
-                        src="/IMG_4202.jpg"
-                    />
-                </div>
-                <div className="flex flex-col gap-1 justify-center items-center">
-                    <h2 className="fade-in text-xl" style={{
-                        animationDuration: '1000ms',
-                        animationDelay: '200ms'
-                    }}>
-                        Frontend Developer | Software Engineer
-                    </h2>
-                    <h1 className="text-5xl fade-in bg-clip-text text-transparent"
+        <>
+            <motion.div className="hidden md:flex sticky top-0 z-0 gap-10 flex-col md:flex-row md:h-screen h-[calc(100vh-155px)] w-full pt-15" style={{
+                opacity: opacityScale,
+                scale: scaleScale,
+                filter: blurScale && useTransform(blurScale, (b) => `blur(${b})`)
+            }}>
+                {SHOW_CONTAINER_NAMES && <p className="absolute text-red-500 font-bold">container 1</p>}
+                <div className="flex items-center justify-center flex-col flex-1 rounded-xl min-h-100 gap-8">
+                    <div className="flex  h-50 w-50 md:h-50 md:w-50 fade-in rounded-full overflow-hidden justify-center items-center bg-white"
                         style={{
                             animationDuration: '1000ms',
-                            animationDelay: '400ms',
-                            backgroundImage: 'linear-gradient(to right, #59B5BD, #42a1e0ff, #0f576dff)'
                         }}>
-                        Jordan Dexter
-                    </h1>
-                    <div className="grid z-10 grid-cols-2 pt-4 gap-4 fade-in"
-                        style={{
+                        <img className="object-cover h-50 w-50"
+                            src="/IMG_4202.jpg"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-1 justify-center items-center">
+                        <h2 className="fade-in text-xl" style={{
                             animationDuration: '1000ms',
-                            animationDelay: '600ms',
+                            animationDelay: '200ms'
                         }}>
-                        <button className="flex bg-gray-500 transition-all duration-300 min-w-[150px] rounded-full text-gray-200 font-bold hover:bg-gray-600 hover:text-white"
-                            onClick={() => { scrollToAbout() }}>
-                            About
-                        </button>
-                        <button className="flex bg-primary transition-all duration-300 min-w-[150px] rounded-full text-gray-200 py-2 font-bold hover:bg-primary-hover hover:text-white"
-                            onClick={onOpenContactModal}>
-                            Contact
-                        </button>
+                            Frontend Developer | Software Engineer
+                        </h2>
+                        <h1 className="text-5xl fade-in bg-clip-text text-transparent"
+                            style={{
+                                animationDuration: '1000ms',
+                                animationDelay: '400ms',
+                                backgroundImage: 'linear-gradient(to right, #59B5BD, #42a1e0ff, #0f576dff)'
+                            }}>
+                            Jordan Dexter
+                        </h1>
+                        <div className="grid z-10 grid-cols-2 pt-4 gap-4 fade-in"
+                            style={{
+                                animationDuration: '1000ms',
+                                animationDelay: '600ms',
+                            }}>
+                            <button className="flex bg-gray-500 transition-all duration-300 min-w-[150px] rounded-full text-gray-200 font-bold hover:bg-gray-600 hover:text-white"
+                                onClick={() => { scrollToAbout() }}>
+                                About
+                            </button>
+                            <button className="flex bg-primary transition-all duration-300 min-w-[150px] rounded-full text-gray-200 py-2 font-bold hover:bg-primary-hover hover:text-white"
+                                onClick={onOpenContactModal}>
+                                Contact
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
 
-            <motion.div
-                className="absolute w-screen max-w-[800px] h-[800px] bottom-[-550px] right-1/2 translate-x-1/2 rounded-full opacity-40 animate-pulse fade-in-ball"
-                style={{
-                    background: 'radial-gradient(circle, var(--hero-glow), #ffffff00)',
-                    filter: 'blur(30px)',
-                    animationDelay: '1000ms'
-                }}
-            />
-        </motion.div>
+                <motion.div
+                    className="absolute w-screen max-w-[800px] h-[800px] bottom-[-550px] right-1/2 translate-x-1/2 rounded-full opacity-40 animate-pulse fade-in-ball"
+                    style={{
+                        background: 'radial-gradient(circle, var(--hero-glow), #ffffff00)',
+                        filter: 'blur(30px)',
+                        animationDelay: '1000ms'
+                    }}
+                />
+            </motion.div>
+
+            <HeroMobileHeader
+                scrollRef={scrollRef}
+                aboutSectionRef={aboutSectionRef}
+                onOpenContactModal={onOpenContactModal} />
+        </>
     )
 }
