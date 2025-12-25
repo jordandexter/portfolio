9604619ce9ce601a4b739b58ea1884cddb436177
jordandexter <jdbdexter@gmail.com>
2025-12-24 16:04:54 -0800

    pushin

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { RefObject, useEffect, useRef } from "react"
+import { RefObject, useEffect, useRef, useState } from "react"
 import { motion, useScroll, useTransform } from "framer-motion"
 import { useModalStore } from "@/stores/modalStore";
 import { useFocusedSection } from "@/stores/focusedSection";
+import { Download } from "lucide-react";
 
 const SHOW_CONTAINER_NAMES = false
 
@@ -16,6 +17,8 @@ export default function HeroHeader({
     aboutSectionRef,
 }: HeroHeaderProps) {
     const { setContactFormModalOpen } = useModalStore()
+    const [hovered, setHovered] = useState(false)
+    const [loading, setLoading] = useState(false)
     const { setFocusedSection } = useFocusedSection()
     const triggerRef = useRef<HTMLDivElement | null>(null);
     const { scrollYProgress } = useScroll({
@@ -97,13 +100,25 @@ export default function HeroHeader({
                                 animationDelay: '600ms',
                             }}>
                             <button className="flex bg-gray-500 transition-all duration-300 min-w-[150px] rounded-full text-gray-200 font-bold hover:bg-gray-600 hover:text-white"
-                                onClick={() => { scrollToAbout() }}>
-                                About
-                            </button>
-                            <button className="flex bg-primary transition-all duration-300 min-w-[150px] rounded-full text-gray-200 py-2 font-bold hover:bg-primary-hover hover:text-white"
-                                onClick={() => setContactFormModalOpen(true)}>
+                                onClick={() => { setContactFormModalOpen(true) }}>
                                 Contact
                             </button>
+                            <a className="flex" href="Jordan_Dexter.pdf" download={'Jordan_Dexter.pdf'}
+                                onClick={() => {
+                                    if (loading) return;
+                                    setLoading(true)
+                                    setTimeout(() => {
+                                        setLoading(false)
+                                    }, 2000)
+                                }}
+                                onMouseEnter={() => setHovered(true)}
+                                onMouseLeave={() => setHovered(false)}
+                                title="Download PDF">
+                                <button className="flex bg-primary transition-all duration-300 min-w-[150px] rounded-full text-gray-200 py-2 font-bold hover:bg-primary-hover hover:text-white">
+                                    {loading ? 'Downloading...' : 'Resume'}
+                                    <Download className={`${loading ? 'hidden' : ''} transition-all duration-300 ${hovered ? 'ml-2 w-4 h-4' : 'w-0 h-0'}`} />
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div>
